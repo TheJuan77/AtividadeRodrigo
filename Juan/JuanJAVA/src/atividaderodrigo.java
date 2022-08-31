28473f0b47cdb943ad4ad7be470f85484107a596
Juan Pablo <112036218+TheJuan77@users.noreply.github.com>
2022-08-31 20:41:40 -0300

    Add files via upload

---
+++ Juan/JuanJAVA/src/atividaderodrigo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class atividaderodrigo {
+	public static void main(String[] args) {
+	    
+		 Scanner Juan = new Scanner(System.in);
+		       double media, n1,n2;
+		        
+		        System.out.println("Insira a nota 1(Peso 2): ");
+		       n1 = Juan.nextDouble();
+		        
+		        System.out.println("Insira a nota 2(Peso 3):");
+		       n2 = Juan.nextDouble();
+		        media=(n1+n2)/2;
+		        
+		        System.out.println("Essa é a média do aluno: " + (n1*2 + n2*3)/5);
+		        
+		        
+		    }
+
+		   
+		}
+
+
